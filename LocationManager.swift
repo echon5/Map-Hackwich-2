06159ef41d5973224dddf8a67c9fbd9261cd7544
Erin Chon <echon@bsd220.org>
2023-02-08 10:09:16 -0600

    added user location

---
+++ LocationManager.swift
@@ -0,0 +1,22 @@
+//
+//  LocationManager.swift
+//  Map Hackwich 2
+//
+//  Created by Erin Chon on 2/8/23.
+//
+
+import Foundation
+import CoreLocation
+
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject{
+    var locationManager = CLLocationManager()
+    
+    override init() {
+           super.init()
+           locationManager.delegate = self
+           locationManager.requestWhenInUseAuthorization()
+           locationManager.startUpdatingLocation()
+
+       }
+}
--- Map Hackwich 2/ContentView.swift
+++ Map Hackwich 2/ContentView.swift
@@ -7,20 +7,28 @@
 
 import SwiftUI
 import MapKit
+
 struct ContentView: View {
-    
+    @StateObject var locationManager = LocationManager()
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(
             latitude: 42.15704,
             longitude: -88.14812),
         span: MKCoordinateSpan(
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05)
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005)
     )
     var body: some View {
-        Map(coordinateRegion: $region)
+        Map(
+            coordinateRegion: $region,
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode
+        )
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
