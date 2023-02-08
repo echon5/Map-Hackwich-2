f1059c2a0ddd1b4a226262b8583e42d3ee59438e
Erin Chon <echon@bsd220.org>
2023-02-08 09:52:52 -0600

    Initial Commit

---
+++ Map Hackwich 2/Map_Hackwich_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Map_Hackwich_2App.swift
+//  Map Hackwich 2
+//
+//  Created by Erin Chon on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_Hackwich_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
